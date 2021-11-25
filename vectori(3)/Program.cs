09aa8a58b2e3cdf54b6430bee320d489347994b2
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2021-11-25 17:00:02 +0200

    Hello

---
+++ vectori(3)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vectori_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
