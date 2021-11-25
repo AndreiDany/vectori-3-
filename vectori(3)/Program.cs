6055ba15d2a29ffac3619e954631e95b5e78143e
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2021-11-25 17:07:50 +0200

    Hello

--- vectori(3)/Program.cs
+++ vectori(3)/Program.cs
@@ -6,7 +6,23 @@ namespace vectori_3_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = new int[100];
+            int n;
+            int egale = 0;
+            n = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+                nums[i] = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            { for (int j = i + 1; j <= n; j++)
+                {
+                    if (nums[i] == nums[j]) egale++;
+                }
+
+            }
+            Console.WriteLine(egale);
+
         }
     }
 }
